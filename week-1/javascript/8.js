dbf9d8e9dc101ec32d4a3f8ea0c148be8f9dc866
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-29 18:47:35 +0800

    [JS-8] function that remove all the vowel froma string

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -3,3 +3,10 @@
  * @param {string} str The string to check.
  * @return {string} The string with all vowels removed.
  */
+
+function removeVowel(str) {
+    let NoVowelStr = str.replace(/[aeiou]/gi, '')
+    return NoVowelStr;
+}
+
+console.log(removeVowel("Sheldon Pogi"));
