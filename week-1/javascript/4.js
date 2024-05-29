bf3dcc9a39c41f1a7613ad02c6a01510c63bca41
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-29 18:01:28 +0800

    [JS-4] function that return current date

--- week-1/javascript/4.js
+++ week-1/javascript/4.js
@@ -6,12 +6,13 @@
 
 function getCurrentDate(){
     const today = new Date();
-    const YYYY = today.getFullYear();
-    let MM = today.getMonth() + 1;
-    let DD = today.getDate();
-    let HH = today.getHours();
-    let mm = today.getMinutes();
-    let SS = today.getSeconds();
+    const YYYY = today.getFullYear().toString();
+    let MMplus1 = today.getMonth() + 1;
+    let MM = MMplus1.toString()
+    let DD = today.getDate().toString();
+    let HH = today.getHours().toString();
+    let mm = today.getMinutes().toString();
+    let SS = today.getSeconds().toString();
     return MM +'-'+ DD + '-' + YYYY + ' ' + HH +':'+ mm + ':' + SS;
 }
 
