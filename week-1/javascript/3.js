0e0b0ce47ffce872bba11e6d21ec0cd3ace267e2
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-29 18:19:13 +0800

    [JS-3] function printFizzBuzz

--- week-1/javascript/3.js
+++ week-1/javascript/3.js
@@ -7,14 +7,16 @@
  */
 
 function PrintFizzBuzz(number) {
-    if(number % 3 == 0 && number % 5 == 0){
-        console.log("FizzBuzz");
-    } else if(number % 3 == 0) {
-        console.log("Fizz");
-    } else if(number % 5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(number);
-    }
+    for(let i = 1 ; i <= number; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 == 0) {
+            console.log("Fizz");
+        } else if(i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }    
 }
-PrintFizzBuzz(36);
+PrintFizzBuzz(15);
