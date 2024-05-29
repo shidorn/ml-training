23ffb241012d87476e66dddf586791a00d549046
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-29 17:38:08 +0800

    [JS-3] function printFizzBuzz

--- week-1/javascript/3.js
+++ week-1/javascript/3.js
@@ -5,3 +5,16 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+function PrintFizzBuzz(number) {
+    if(number % 3 == 0 && number % 5 == 0){
+        console.log("FizzBuzz");
+    } else if(number % 3 == 0) {
+        console.log("Fizz");
+    } else if(number % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(number);
+    }
+}
+PrintFizzBuzz(36);
