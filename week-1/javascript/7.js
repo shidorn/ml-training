614a5368506fe8c46fb64db37d4e1d51c3a0699d
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-29 18:39:24 +0800

    [JS-7] function that throws error if param is not an integer

--- week-1/javascript/7.js
+++ week-1/javascript/7.js
@@ -3,3 +3,10 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+function checkIfNumber(num) {
+    if(num === parseInt(num)) return true;
+    throw "Error: parameter is not an integer";
+}
+
+console.log(checkIfNumber(1));
