514024f9398dc8cd9f8e979e6e4b4ba4b30515e7
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-30 14:45:01 +0800

    [JS-18] Shopping cart add and remove

--- week-1/javascript/18.js
+++ week-1/javascript/18.js
@@ -16,3 +16,50 @@
  * @param {string[]} items
  * @returns {string}
  */
+
+let cart = [
+    {name: "Banana", price: 123},
+    {name: "PineApple", price: 456},
+    {name: "WaterMelon", price: 789},
+    {name: "Orange", price: 987},
+];
+
+function AddToCart(items){
+    if(cart.length == 10){
+        return "Cart is full.";
+    }
+    cart.push(items);
+
+    return cart;
+}
+
+function removeFromCart(name){
+    for (let i = 0; i < cart.length; i++) {
+        if(cart[i].name.toLowerCase() == name.toLowerCase()){
+            cart = cart.filter(obj => obj.name.toLocaleLowerCase() !== name.toLowerCase());
+        }
+    }
+    return cart;
+}
+
+function totalPrice() {
+    let TotalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        TotalPrice += cart[i].price
+    }
+    return TotalPrice;
+}
+
+function totalItems(){
+    return cart.length;
+}
+
+// console.log(AddToCart(
+//     {name:"Apple", price:200}
+// ));
+
+// console.log(removeFromCart("PineApple"))
+
+// console.log(totalPrice());
+
+console.log(totalItems());
