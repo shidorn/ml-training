c456fd5a5bff5811777d74ab806edd303efd1a09
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-30 10:37:59 +0800

    [JS-12] Function that returns Error if array is empty

--- week-1/javascript/12.js
+++ week-1/javascript/12.js
@@ -3,3 +3,10 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+function arrNumber(arr) {
+    if(arr.length == 0) throw {ErrorCode : 0 ,Message: "Array is empty"};
+    return arr;
+}
+
+console.log(arrNumber([]));
