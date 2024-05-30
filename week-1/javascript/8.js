8e0e0f884c04a73cb89d011cdf3861e0940f5d3b
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-30 10:15:28 +0800

    [JS-8] Function that returns all Vowels in a string

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -3,9 +3,15 @@
  * @param {string} str The string to check.
  * @return {string} The string with all vowels.
  */
-function removeVowel(str) {
-    let NoVowelStr = str.replace(/[aeiou]/gi, '')
-    return NoVowelStr;
+function VowelsInStr(str) {
+    let vowels = "";
+    for(let i =0; i < str.length; i++) {
+        let letter = str[i].toLowerCase();
+        if("aeiou".includes(letter)){
+            vowels += letter;
+        }
+    }
+    return vowels;
 }
 
-console.log(removeVowel("Sheldon Pogi"));
+console.log(VowelsInStr("qwertasdfg"));
