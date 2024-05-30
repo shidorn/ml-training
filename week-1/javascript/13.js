31cda8617c92d8d222e1eb7ec4deb804e48d1729
shidorn <78392833+shidorn@users.noreply.github.com>
2024-05-30 10:47:09 +0800

    [JS-13] function that return Uppercase every word

--- week-1/javascript/13.js
+++ week-1/javascript/13.js
@@ -6,3 +6,14 @@
  * Example string: "The quick brown fox jumps over the lazy dog"
  * Expected output: "The Quick Brown Fox Jumps Over The Lazy Dog"
  */
+
+function Ucase1stLetterEvryWrd(str) {
+    let string = str.toLowerCase().split(' ');
+    console.log(string.length);
+    for (let i = 0; i < string.length; i++) {
+        string[i] = string[i].charAt(0).toUpperCase() + string[i].substring(1);
+    }
+    return string.join(' ');
+}
+
+console.log(Ucase1stLetterEvryWrd("the quick brown fox jumps over the lazy dog"))
