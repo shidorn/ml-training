cc50a1ad0f25404460c772237c8f741d3a6fc671
Rafael Seva <rafaelsevaiv@gmail.com>
2024-06-01 00:45:07 +0800

    [JS-15] function that logs if number is even or odd

--- week-1/javascript/15.js
+++ week-1/javascript/15.js
@@ -2,3 +2,12 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+
+const isEvenOrOdd = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) console.log(`${i} is even.`);
+    else console.log(`${i} is odd.`);
+  }
+};
+
+isEvenOrOdd(5);
