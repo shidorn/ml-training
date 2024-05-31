46e5ce53114ae7074e716e0605fc5e2ac022f8dd
Rafael Seva <rafaelsevaiv@gmail.com>
2024-06-01 00:33:33 +0800

    [JS-7] function to check if number is integer

--- week-1/javascript/7.js
+++ week-1/javascript/7.js
@@ -3,3 +3,15 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+const isInteger = (num) => {
+  if (!Number.isInteger(num)) {
+    throw new Error(`isInteger(${num}) parameter must be an integer`);
+  }
+
+  return Number.isInteger(num);
+};
+
+console.log(isInteger(1)); // true
+console.log(isInteger(1.1)); // false - error
+console.log(isInteger("1")); // false - error
