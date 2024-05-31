faa815abb52d3feff20da36d5a8f9a74089241f3
Rafael Seva <rafaelsevaiv@gmail.com>
2024-06-01 00:22:37 +0800

    [JS-2] function to sum all numbers in an array

--- week-1/javascript/2.js
+++ week-1/javascript/2.js
@@ -3,3 +3,7 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+const sum = (arr) => arr.reduce((a, b) => a + b, 0);
+
+console.log(sum([1, 2, 3, 4, 5]));
