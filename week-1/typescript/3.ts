b79243d517e97ac024473709ec1c664c43d81c91
shidorn <78392833+shidorn@users.noreply.github.com>
2024-06-01 13:49:27 +0800

    [TS-3] function that prints FizzBuzz

--- week-1/typescript/3.ts
+++ week-1/typescript/3.ts
@@ -5,3 +5,18 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+function PrintFizzBuzzTS(n : number): void {
+    for(let i = 1 ; i <= n; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 == 0) {
+            console.log("Fizz");
+        } else if(i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }    
+}
+PrintFizzBuzzTS(20);
