77542e40a35cbe93b5f7ee31bb6b586d9b592528
shidorn <78392833+shidorn@users.noreply.github.com>
2024-06-01 15:34:19 +0800

    [TS-18] function that add,remove,display prices of shopping cart

--- week-1/typescript/18.js
+++ week-1/typescript/18.js
@@ -17,3 +17,38 @@
  * @param {string[]} items
  * @returns {string}
  */
+var cart = [
+    { name: "Banana", price: 123 },
+    { name: "PineApple", price: 456 },
+    { name: "WaterMelon", price: 789 },
+    { name: "Orange", price: 987 },
+];
+function AddToCart(items) {
+    if (cart.length == 10) {
+        return "Cart is full.";
+    }
+    cart.push(items);
+    return cart;
+}
+function removeFromCart(items) {
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].name.toLowerCase() == items.name.toLowerCase()) {
+            cart = cart.filter(function (obj) { return obj.name.toLocaleLowerCase() !== items.name.toLowerCase(); });
+        }
+    }
+    return cart;
+}
+function totalPrice() {
+    var TotalPrice = 0;
+    for (var i = 0; i < cart.length; i++) {
+        TotalPrice += cart[i].price;
+    }
+    return TotalPrice;
+}
+function totalItems() {
+    return cart.length;
+}
+console.log(AddToCart({ name: "Apple", price: 200 }));
+// console.log(removeFromCart({ name: "PineApple", price:  456}));
+// console.log(totalPrice());
+// console.log(totalItems());
--- week-1/typescript/18.ts
+++ week-1/typescript/18.ts
@@ -16,3 +16,55 @@
  * @param {string[]} items
  * @returns {string}
  */
+
+interface CartItem {
+    name: string;
+    price: number;
+}
+
+let cart: CartItem[] = [
+    {name: "Banana", price: 123},
+    {name: "PineApple", price: 456},
+    {name: "WaterMelon", price: 789},
+    {name: "Orange", price: 987},
+];
+
+function AddToCart(items: CartItem): CartItem[] | string{
+    if(cart.length == 10){
+        return "Cart is full.";
+    }
+    cart.push(items);
+
+    return cart;
+}
+
+function removeFromCart(items: CartItem): CartItem[] | string{
+    for (let i = 0; i < cart.length; i++) {
+        if(cart[i].name.toLowerCase() == items.name.toLowerCase()){
+            cart = cart.filter(obj => obj.name.toLocaleLowerCase() !== items.name.toLowerCase());
+        }
+    }
+    return cart;
+}
+
+function totalPrice() {
+    let TotalPrice = 0;
+    for (let i = 0; i < cart.length; i++) {
+        TotalPrice += cart[i].price
+    }
+    return TotalPrice;
+}
+
+function totalItems(){
+    return cart.length;
+}
+
+console.log(AddToCart(
+    {name:"Apple", price:200}
+));
+
+// console.log(removeFromCart({ name: "PineApple", price:  456}));
+
+// console.log(totalPrice());
+
+// console.log(totalItems());
