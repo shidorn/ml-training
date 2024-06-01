f66c27a54ccdea14539ffae7a089a4c6358c7e61
shidorn <78392833+shidorn@users.noreply.github.com>
2024-06-01 10:47:03 +0800

    [TS-1] function that returns odd and even numbers

--- week-1/typescript/1.ts
+++ week-1/typescript/1.ts
@@ -3,3 +3,11 @@
  * @param {number} num The number to check.
  * @return {boolean} True if the number is even, false if it is odd.
  */
+
+function EvenOddIdentifier(num: number): boolean {
+    if(num % 2 == 0){
+        return true;
+    }
+    return false;
+}
+console.log(EvenOddIdentifier(5));
