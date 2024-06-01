3856cafb4620be16717f614e31122a14766715e1
shidorn <78392833+shidorn@users.noreply.github.com>
2024-06-01 13:51:21 +0800

    [TS-4] function that returns current date

--- week-1/typescript/4.ts
+++ week-1/typescript/4.ts
@@ -3,3 +3,17 @@
  * 'MM-DD-YYYY HH:MM:SS'.
  * @return {string} The current date and time in the format 'MM-DD-YYYY HH:MM:SS'.
  */
+
+function getCurrentDateTS(){
+    const today = new Date();
+    const YYYY = today.getFullYear().toString();
+    let MMplus1 = today.getMonth() + 1;
+    let MM = MMplus1.toString()
+    let DD = today.getDate().toString();
+    let HH = today.getHours().toString();
+    let mm = today.getMinutes().toString();
+    let SS = today.getSeconds().toString();
+    return MM +'-'+ DD + '-' + YYYY + ' ' + HH +':'+ mm + ':' + SS;
+}
+
+console.log(getCurrentDateTS());
